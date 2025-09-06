a110832e4297bd09318813c8eec44d194d4c29b2
Derek Li <derekli@MacBookAir.hsd1.wa.comcast.net>
2025-09-06 11:06:29 -0700

    get server working

--- python/server.py
+++ python/server.py
@@ -251,4 +251,4 @@ async def analyze_video(file: fastapi.UploadFile):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8800)
