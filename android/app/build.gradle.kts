809d8d906b11c900da5a3cf7c264c219c4d7c1ad
Austin A. <prodbyskynse@gmail.com>
2025-10-12 19:05:42 -0500

    change package name to derek li

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,7 @@ if (keystorePropertiesFile.exists()) {
     keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
 android {
-    namespace = "com.example.hooplab"
+    namespace = "com.derekli.hooplab"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "28.0.13004108"
 
@@ -30,7 +30,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.hooplab"
+        applicationId = "com.derekli.hooplab"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/hooplab/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/hooplab/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.hooplab
+package com.derekli.hooplab
 
 import io.flutter.embedding.android.FlutterActivity
 
